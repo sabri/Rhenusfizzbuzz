0890243c38066908b4673299e8750d5a169d0cfc
sabri baazaoui <baazaouisabri@gmail.com>
2020-09-24 15:19:02 +0100

    Completed Controller

--- src/main/java/com/rhenus/fizzbuzz/fizzbuzz/FizzBuzzController.java
+++ src/main/java/com/rhenus/fizzbuzz/fizzbuzz/FizzBuzzController.java
@@ -1,17 +1,27 @@
 package com.rhenus.fizzbuzz.fizzbuzz;
 
 
+import com.rhenus.fizzbuzz.fizzbuzz.FizzBuzzService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/fizzbuzz/")
+@RequestMapping(value = "/api/v1/fizzbuzz/")
 public class FizzBuzzController {
-    @GetMapping("number/{input}")
-    public String getNumber(@PathVariable int input){
-        return "number!";
+
+    private final FizzBuzzService service;
+
+    @Autowired
+    public FizzBuzzController(FizzBuzzService service) {
+        this.service = service;
     }
 
+
+    @GetMapping("number/{input}")
+    public String getNumber(@PathVariable int input){
+        return service.playinggame(input);
+    }
 }
