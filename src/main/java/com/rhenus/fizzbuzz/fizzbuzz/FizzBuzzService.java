ddd2191f75a8d971d99b9430e9701407cfff9493
sabri baazaoui <baazaouisabri@gmail.com>
2020-09-24 15:14:11 +0100

    Failed test next step to test should pass

---
+++ src/main/java/com/rhenus/fizzbuzz/fizzbuzz/FizzBuzzService.java
@@ -0,0 +1,11 @@
+package com.rhenus.fizzbuzz.fizzbuzz;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class FizzBuzzService {
+
+    public String playinggame(int input){
+        return null;
+    }
+}
--- src/test/java/com/rhenus/fizzbuzz/fizzbuzz/FizzBuzzControllerTest.java
+++ src/test/java/com/rhenus/fizzbuzz/fizzbuzz/FizzBuzzControllerTest.java
@@ -5,8 +5,12 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -16,11 +20,15 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class FizzBuzzControllerTest {
     @Autowired
     private MockMvc mockMvc;
+    @MockBean
+    private FizzBuzzService service;
 
     @Test
-    public void getNumber() throws Exception {
-        mockMvc.perform(get("/api/fizzbuzz/number/42")).andExpect(status().isOk())
-                .andExpect(content().string("number!"));
+    void getNumber() throws Exception{
+        when(service.playinggame(anyInt())).thenReturn("number!");
+        mockMvc.perform(get("/api/v1/fizzbuzz/number/42"))
+                .andExpect(status().isOk()).andExpect(content().string("number"));
+        verify(service, times(1)).playinggame(42);
     }
 
 }
