b796be54aeed122582cd93df370d9ec07d107bbb
sabri baazaoui <baazaouisabri@gmail.com>
2020-09-24 15:32:18 +0100

    Refactoring Code

--- src/main/java/com/rhenus/fizzbuzz/fizzbuzz/FizzBuzzService.java
+++ src/main/java/com/rhenus/fizzbuzz/fizzbuzz/FizzBuzzService.java
@@ -2,10 +2,28 @@ package com.rhenus.fizzbuzz.fizzbuzz;
 
 import org.springframework.stereotype.Service;
 
+
 @Service
 public class FizzBuzzService {
 
     public String playinggame(int input){
-        return "1";
+
+        if (isDivisibleBy(input,15)){
+            return "FizzBuzz";
+        }
+
+        if (isDivisibleBy(input,3)){
+            return "Fizz";
+        }
+        if (isDivisibleBy(input,5)){
+            return "Buzz";
+        }
+
+        return String.valueOf(input);
+    }
+
+    private boolean isDivisibleBy(int input, int i) {
+        return input % i == 0;
     }
 }
+
--- src/test/java/com/rhenus/fizzbuzz/fizzbuzz/FizzBuzzServiceTest.java
+++ src/test/java/com/rhenus/fizzbuzz/fizzbuzz/FizzBuzzServiceTest.java
@@ -1,7 +1,10 @@
 package com.rhenus.fizzbuzz.fizzbuzz;
 
-import org.junit.jupiter.api.Test;
+
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -12,12 +15,32 @@ import static org.assertj.core.api.Assertions.assertThat;
 @ExtendWith(SpringExtension.class)
 class FizzBuzzServiceTest {
     @Autowired
-    private  FizzBuzzService service;
+    private FizzBuzzService service;
 
+    @ParameterizedTest
+    @CsvSource(value = {"1,1","2,2"})
+    void test_normal_number(int input, String expected) {
+        assertThat(service.playinggame(input)).isEqualTo(expected);
 
-    @Test
-    void first_test_number() {
-        assertThat(service.playinggame(1)).isEqualTo("1");
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {3,6})
+    void divideby3_normal_number(int input) {
+        assertThat(service.playinggame(input)).isEqualTo("Fizz");
+
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {5,10})
+    void divided5_normal_number(int input) {
+        assertThat(service.playinggame(input)).isEqualTo("Buzz");
+
+    }
+    @ParameterizedTest
+    @ValueSource(ints = {15,30})
+    void divideby15_normal_number(int input) {
+        assertThat(service.playinggame(input)).isEqualTo("FizzBuzz");
 
     }
 }
